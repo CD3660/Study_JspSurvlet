be715658084231e09fbd0c8dee181a36340e2b85
박찬영 <144075327+CD3660@users.noreply.github.com>
2023-12-11 18:34:27 +0900

    Update MemberController.java

--- 05.MybatisMVC/src/main/java/controller/MemberController.java
+++ 05.MybatisMVC/src/main/java/controller/MemberController.java
@@ -18,37 +18,37 @@ import member.MemberDAO_Id;
 import member.MemberService;
 import member.MemberVO;
 
-
 @WebServlet("*.me")
 public class MemberController extends HttpServlet {
 	RequestDispatcher rd;
 	MemberService service;
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String path = req.getServletPath();
-		if(path.equals("/loginpage.me")) {
+		if (path.equals("/loginpage.me")) {
 			rd = req.getRequestDispatcher("member/login.jsp");
 			rd.forward(req, resp);
-		} else if(path.equals("/login.me")) {
+		} else if (path.equals("/login.me")) {
 			service = new MemberDAO();
 			MemberVO vo = new MemberVO();
 			vo.setUser_id(req.getParameter("user_id"));
 			vo.setUser_pw(req.getParameter("user_pw"));
-			
+
 			vo = service.member_login(vo);
-			if(vo == null) {
+			if (vo == null) {
 				resp.getWriter().print("로그인 실패");
 			} else {
 				req.getSession().setAttribute("loginInfo", vo);
-				resp.getWriter().print("로그인 성공 "+vo.getName()+"님");
+				resp.getWriter().print("로그인 성공 " + vo.getName() + "님");
 			}
-		} else if(path.equals("/logout.me")) {
+		} else if (path.equals("/logout.me")) {
 			req.getSession().removeAttribute("loginInfo");
 			resp.sendRedirect("/mvc");
-		} else if(path.equals("/joinpage.me")) {
+		} else if (path.equals("/joinpage.me")) {
 			rd = req.getRequestDispatcher("member/join.jsp");
 			rd.forward(req, resp);
-		} else if(path.equals("/join.me")) {
+		} else if (path.equals("/join.me")) {
 			MemberVO vo = new MemberVO();
 			vo.setUser_id(req.getParameter("user_id"));
 			vo.setUser_pw(req.getParameter("user_pw"));
@@ -60,17 +60,10 @@ public class MemberController extends HttpServlet {
 			vo.setPost(req.getParameter("post"));
 			service = new MemberDAO();
 			System.out.println(service.member_join(vo));
-			
-		} else if(path.equals("/idCheck.me")) {
+
+		} else if (path.equals("/idCheck.me")) {
 			service = new MemberDAO_Id();
-			
-			if(service.member_idCheck(req.getParameter("user_id"))==0) {
-				System.out.println(0);
-				resp.getWriter().print("0");
-			} else {
-				System.out.println(1);
-				resp.getWriter().print("1");
-			}
+			resp.getWriter().print(service.member_idCheck(req.getParameter("user_id")));
 		}
 	}
 }
